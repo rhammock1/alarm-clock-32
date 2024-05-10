8c82596430d4b32fd60ad5b4ec31e60151410e65
rhammock1 <robert.hammock@gotab.io>
2024-05-10 12:40:33 -0400

    move config to global scope and expose functions

--- main/drivers/lilfs.c
+++ main/drivers/lilfs.c
@@ -3,6 +3,25 @@
 
 static const char *TAG = "LILFS";
 
+lfs_t lfs;
+
+struct lfs_config w25q128_cfg = {
+  .context = NULL,
+  // block device operations
+  .read = NULL,
+  .prog = NULL,
+  .erase = NULL,
+  .sync = NULL,
+  // block device configuration
+  .read_size = 1,
+  .prog_size = 1,
+  .block_size = 4096,
+  .block_count = 4096,
+  .lookahead_size = 16,
+  .cache_size = 16,
+  .block_cycles = 500,
+};
+
 int w25q128_lfs_read(const struct lfs_config *c, lfs_block_t block,
         lfs_off_t off, void *buffer, lfs_size_t size) {
     spi_device_handle_t handle = (spi_device_handle_t)c->context;
@@ -69,43 +88,103 @@ int w25q128_lfs_erase(const struct lfs_config *c, lfs_block_t block) {
 }
 
 int w25q128_lfs_sync(const struct lfs_config *c) {
-    return 0;
+  return 0;
+}
+
+esp_err_t format_lfs() {
+  ESP_LOGE(TAG, "Attempting format");
+  // If the mount failed, format the file system and try again
+  lfs_format(&lfs, &w25q128_cfg);
+  return ESP_OK;
+}
+
+esp_err_t format_and_mount_lfs() {
+  // If the mount failed, format the file system and try again
+  lfs_format(&lfs, &w25q128_cfg);
+  int err = lfs_mount(&lfs, &w25q128_cfg);
+  if (err) {
+      // If the mount still fails, there's a serious problem
+      ESP_LOGE(TAG, "Failed to mount or format LittleFS");
+      return ESP_FAIL;
+  }
+  return ESP_OK;
+}
+
+int lfs_open(lfs_file_t *file, const char *path, int flags) {
+  int err = lfs_file_open(&lfs, file, path, flags);
+  if (err) {
+    if(err == LFS_ERR_CORRUPT) {
+      ESP_LOGE(TAG, "Corrupt filesystem, formatting and trying again");
+      format_and_mount_lfs();
+      err = lfs_file_open(&lfs, file, path, flags);
+      if(err) {
+        ESP_LOGE(TAG, "Error opening file: %d", err);
+        return ESP_FAIL;
+      }
+    } else {
+      ESP_LOGE(TAG, "Error while attempting to open file: %d", err);
+      return err;
+    }
+  }
+  return 0;
+}
+
+void lfs_close(lfs_file_t *file) {
+  lfs_file_close(&lfs, file);
+}
+
+void lfs_write(lfs_file_t *file, const void *buffer, size_t size) {
+  lfs_file_write(&lfs, file, buffer, size);
+}
+
+int lfs_read(lfs_file_t *file, void *buffer, size_t size) {
+  return lfs_file_read(&lfs, file, buffer, size);
+}
+
+int lfs_read_string(lfs_file_t *file, char *buffer, size_t size) {
+  if (size == 0) {
+    return LFS_ERR_INVAL;
+  }
+
+  int result = lfs_read(file, buffer, size - 1);
+  if (result < 0) {
+    return result;
+  }
+
+  buffer[result] = '\0';
+  return result;
+}
+
+esp_err_t mount_lfs() {
+  ESP_LOGI(TAG, "Attempting to mount littleFS");
+  int err = lfs_mount(&lfs, &w25q128_cfg);
+  if (err) {
+    format_and_mount_lfs();
+  }
+
+  return ESP_OK;
+}
+
+esp_err_t unmount_lfs() {
+  int err = lfs_unmount(&lfs);
+  if (err) {
+    ESP_LOGE(TAG, "Error unmounting LittleFS: %d", err);
+    return ESP_FAIL;
+  }
+
+  return ESP_OK;
 }
 
 esp_err_t init_littlefs(spi_device_handle_t handle) {
-  lfs_t lfs;
-  const struct lfs_config cfg = {
-    .context = handle,
-    // block device operations
-    .read = w25q128_lfs_read,
-    .prog = w25q128_lfs_prog,
-    .erase = w25q128_lfs_erase,
-    .sync = w25q128_lfs_sync,
-    // block device configuration
-    .read_size = 1,
-    .prog_size = 1,
-    .block_size = 4096,
-    .block_count = 4096,
-    .lookahead_size = 16,
-    .cache_size = 16,
-    .block_cycles = 500,
-  };
+  w25q128_cfg.context = handle;
+  w25q128_cfg.read = w25q128_lfs_read;
+  w25q128_cfg.prog = w25q128_lfs_prog;
+  w25q128_cfg.erase = w25q128_lfs_erase;
+  w25q128_cfg.sync = w25q128_lfs_sync;
 
   // w25q128_chip_erase(handle);
 
-  ESP_LOGI(TAG, "Attempting to mount littleFS");
-  int err = lfs_mount(&lfs, &cfg);
-  if (err) {
-    ESP_LOGE(TAG, "Attempting format");
-    // If the mount failed, format the file system and try again
-    lfs_format(&lfs, &cfg);
-    err = lfs_mount(&lfs, &cfg);
-    if (err) {
-        // If the mount still fails, there's a serious problem
-        ESP_LOGE(TAG, "Failed to mount or format LittleFS");
-        return ESP_FAIL;
-    }
-  }
+  mount_lfs();
 
   struct lfs_info info;
 
@@ -120,22 +199,46 @@ esp_err_t init_littlefs(spi_device_handle_t handle) {
 
   // add a file
   lfs_file_t file;
-  lfs_file_open(&lfs, &file, "hello.txt", LFS_O_WRONLY | LFS_O_CREAT);
+  int err = lfs_open(&file, "hello.txt", LFS_O_WRONLY | LFS_O_CREAT);
+  if (err) {
+    ESP_LOGE(TAG, "Error opening hello.txt file: %d", err);
+    return ESP_FAIL;
+  }
 
-  lfs_file_write(&lfs, &file, "Hello, LittleFS!", 16);
+  lfs_write(&file, "Hello, LittleFS!", 16);
 
-  lfs_file_close(&lfs, &file);
+  lfs_close(&file);
 
   // read the file
-  char buffer[16];
-  lfs_file_open(&lfs, &file, "hello.txt", LFS_O_RDONLY);
-  lfs_file_read(&lfs, &file, buffer, 16);
-  lfs_file_close(&lfs, &file);
+  char buffer[17];
+  err = lfs_open(&file, "hello.txt", LFS_O_RDONLY);
+  if (err) {
+    ESP_LOGE(TAG, "Error opening for read hello.txt file: %d", err);
+    return ESP_FAIL;
+  }
+  lfs_read_string(&file, buffer, 17);
+  lfs_close(&file);
 
   ESP_LOGI(TAG, "Read from file: %s", buffer);
 
-  // Clean up
-  lfs_unmount(&lfs);
+  // Create the directory
+  int result = lfs_mkdir(&lfs, "/uploads");
+
+  if (result == 0) {
+      printf("Directory created successfully\n");
+  } else {
+      printf("Failed to create directory, error code: %d\n", result);
+  }
+
+  lfs_dir_open(&lfs, &dir, "/uploads");
+  while (lfs_dir_read(&lfs, &dir, &info) > 0) {
+    ESP_LOGI(TAG, "File: %s, Size: %lu", info.name, info.size);
+  }
+
+  lfs_dir_close(&lfs, &dir);
+
+  // // Clean up
+  unmount_lfs();
 
   return ESP_OK;
 }
--- main/include/lilfs.h
+++ main/include/lilfs.h
@@ -2,3 +2,12 @@
 #include "w25q128.h"
 
 esp_err_t init_littlefs(spi_device_handle_t handle);
+int lfs_read_string(lfs_file_t *file, char *buffer, size_t size);
+int lfs_open(lfs_file_t *file, const char *path, int flags);
+void lfs_close(lfs_file_t *file);
+void lfs_write(lfs_file_t *file, const void *buffer, size_t size);
+int lfs_read(lfs_file_t *file, void *buffer, size_t size);
+esp_err_t mount_lfs();
+esp_err_t unmount_lfs();
+esp_err_t format_lfs();
+esp_err_t format_and_mount_lfs();
